0c9974c89556ea4a18fb835f2e6aedfaf7935aba
Tom Eyckmans <teyckmans@gmail.com>
2020-12-30 22:41:53 +0100

    swap blog and help links in header.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,8 +39,8 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {page: 'docs/home.html', label: 'Docs'},
-    {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
+    {page: 'help', label: 'Help'},
   ],
 
   // If you have users set above, you add it here:
